32d99a6f18ec04c4da18fc87daacef6235b08e20
nilanjana20 <nilanjanac873@gmail.com>
2022-02-01 21:28:43 +0530

    Nilanjana added UC1 Simple Linked list program

---
+++ LinkedList.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    public class LinkedList
+    {
+	 internal Node head;
+     internal void Add(int data)
+     {
+         Node node = new Node(data);
+         if (this.head==null)
+         {
+             this.head = node;
+         }
+         else
+         {
+             Node temp = head;
+             while (temp.next != null)
+             {
+                 temp = temp.next;
+             }
+             temp.next = node;
+         }
+         Console.WriteLine("{0} inserted into linked list", node.data);
+     }
+     internal void Display()
+     {
+         Node temp = this.head;
+         if (temp == null)
+         {
+             Console.WriteLine("LinkedList is empty");
+             return;
+         }
+         while ( temp != null )
+         {
+             Console.Write(temp.data + " ");
+             temp = temp.next;
+         }
+     }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+ class Program
+ {
+	    static void Main(string[] args)
+		{
+			LinkedList list = new LinkedList();
+			list.Add(56);
+			list.Add(30);
+			list.Add(70);
+			list.Display();
+		}
+  }
+}
